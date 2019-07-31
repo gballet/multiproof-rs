76a0add16ab7a7596b45bccd6acd4189be96c331
Guillaume Ballet <gballet@gmail.com>
2019-07-31 21:54:23 +0200

    Skeleton for hashing

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
+extern crate rlp;
 extern crate sha3;
 
 use sha3::{Digest, Keccak256};
 
 #[derive(Debug, Clone, PartialEq)]
 enum Node {
-    Hash(Box<Node>, u32, u32), // (Item to hash, # of empty strings, total # of items)
+    Hash(usize, usize, usize), // (Item to hash, # of empty strings, total # of items)
     Leaf(Vec<u8>, Vec<u8>),
     Extension(Vec<u8>, Box<Node>),
     FullNode(Vec<Node>),
@@ -12,7 +13,39 @@ enum Node {
 }
 
 trait Hashable {
-    fn hash(&self) -> Vec<u8>;
+    fn hash(&self, hashers: &mut Vec<Keccak256>) -> Vec<u8>;
+}
+
+impl Hashable for Node {
+    fn hash(&self, hashers: &mut Vec<Keccak256>) -> Vec<u8> {
+        use Node::*;
+        match self {
+            EmptySlot => Vec::new(),
+            Leaf(ref k, ref v) => {
+                let encoding =
+                    rlp::encode_list::<Vec<u8>, Vec<u8>>(&vec![k.clone(), v.clone()][..]);
+                let mut hasher = Keccak256::new();
+                hasher.input(&encoding);
+                Vec::<u8>::from(&hasher.result()[..])
+            }
+            FullNode(ref nodes) => {
+                /* XXX placeholder, incorrect */
+                let mut keys = Vec::new();
+                for node in nodes {
+                    keys.push(node.hash(hashers));
+                }
+                let encoding = rlp::encode_list::<Vec<u8>, Vec<u8>>(&keys[..]);
+                let mut hasher = Keccak256::new();
+                hasher.input(&encoding);
+                Vec::<u8>::from(&hasher.result()[..])
+            }
+            Hash(hridx, _, _) => {
+                let res = hashers[*hridx].clone().result();
+                Vec::<u8>::from(&res[..])
+            }
+            _ => panic!("Attemped to hash invalid type"),
+        }
+    }
 }
 
 enum Instruction {
@@ -27,6 +60,7 @@ fn step(
     stack: &mut Vec<Node>,
     keyvals: Vec<(Vec<u8>, Vec<u8>)>,
     instructions: Vec<Instruction>,
+    mut hashers: &mut Vec<Keccak256>,
 ) -> Node {
     use Instruction::*;
     use Node::*;
@@ -36,7 +70,13 @@ fn step(
         match instr {
             HASHER(digit) => {
                 if let Some(item) = stack.pop() {
-                    stack.push(Hash(Box::new(item), 1 + digit, digit));
+                    let mut hasher = Keccak256::new();
+                    for _ in 1..digit {
+                        hasher.input(b"");
+                    }
+                    hasher.input(item.hash(&mut hashers));
+                    hashers.push(hasher);
+                    stack.push(Hash(hashers.len() - 1, 1 + digit, digit));
                 } else {
                     panic!("Could not pop a value from the stack, that is required for a HASHER")
                 }
@@ -81,7 +121,18 @@ fn step(
                             // a child (el1) of the parent node (el2). this is done during resolve.
                             n2[digit] = el1;
                         }
-                        _ => panic!("Not supported yet"), // Need to support Hash()
+                        Hash(ref hasher_num, ref mut count, ref mut nempty) => {
+                            for _ in 1..digit {
+                                hashers[*hasher_num].input(b"");
+                            }
+                            /* XXX if serialization length is < 32, input that directly */
+                            let el1_hash = el1.hash(&mut hashers);
+                            hashers[*hasher_num].input(el1_hash);
+
+                            *nempty += digit;
+                            *count += digit + 1;
+                        }
+                        _ => panic!("Unexpected node type"),
                     }
                 } else {
                     panic!("Could not find enough parameters to ADD")
@@ -103,10 +154,12 @@ mod tests {
     #[test]
     fn tree_with_just_one_leaf() {
         let mut stack = Vec::new();
+        let mut hashers = Vec::new();
         let out = step(
             &mut stack,
             vec![(vec![1, 2, 3], vec![4, 5, 6])],
             vec![LEAF(0)],
+            &mut hashers,
         );
         assert_eq!(out, Leaf(vec![], vec![4, 5, 6]))
     }
@@ -114,10 +167,12 @@ mod tests {
     #[test]
     fn tree_with_one_branch() {
         let mut stack = Vec::new();
+        let mut hashers = Vec::new();
         let out = step(
             &mut stack,
             vec![(vec![1, 2, 3], vec![4, 5, 6])],
             vec![LEAF(0), BRANCH(0)],
+            &mut hashers,
         );
         assert_eq!(
             out,
@@ -145,6 +200,7 @@ mod tests {
     #[test]
     fn tree_with_added_branch() {
         let mut stack = Vec::new();
+        let mut hashers = Vec::new();
         let out = step(
             &mut stack,
             vec![
@@ -152,6 +208,7 @@ mod tests {
                 (vec![7, 8, 9], vec![10, 11, 12]),
             ],
             vec![LEAF(0), BRANCH(0), LEAF(1), ADD(2)],
+            &mut hashers,
         );
         assert_eq!(
             out,
