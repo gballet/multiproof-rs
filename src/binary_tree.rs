6d22b1aef9548581826b3c04b3e00d6cc709388c
Guillaume Ballet <gballet@gmail.com>
2020-09-10 13:59:55 +0000

    m4: test with a larger, random key set

--- src/binary_tree.rs
+++ src/binary_tree.rs
@@ -355,8 +355,11 @@ impl Default for BinaryExtTree {
 
 #[cfg(test)]
 mod tests {
+    extern crate rand;
+
     use super::BinaryExtTree::*;
     use super::*;
+    use rand::{thread_rng, Rng};
 
     #[test]
     fn simple_insert() {
@@ -510,5 +513,18 @@ mod tests {
             assert_eq!(prefix.len(), 2);
             assert_eq!(m4_hash, l0_hash);
         }
+    }
+
+    #[test]
+    fn many_keys_m4() {
+        let mut root = BinaryExtTree::default();
+        for _ in 0..1000 {
+            let mut key_bytes = vec![0u8; 32];
+            thread_rng().fill(&mut key_bytes[..]);
+            let key = BinaryKey::from(key_bytes);
+            root.insert(&key, vec![5u8; 32]).unwrap();
+        }
+
+        root.hash_m4();
     }
 }
